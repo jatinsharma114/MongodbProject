b42a59bd230e3e64b73be0ae2cf32c972f0f7327
jatinsharma114 <jatin2010sharma@gmail.com>
2024-11-07 18:49:26 +0530

    BranchQA_111 : Added changes at API level.

--- src/main/java/com/test/mongodbTest/Controller/FlightController.java
+++ src/main/java/com/test/mongodbTest/Controller/FlightController.java
@@ -1,13 +1,13 @@
 package com.test.mongodbTest.Controller;
 
 import com.test.mongodbTest.Model.FlightDetails;
+import com.test.mongodbTest.Repository.FlightDetailsRepository;
 import com.test.mongodbTest.Service.FlightDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/flight")
@@ -16,9 +16,25 @@ public class FlightController {
     @Autowired
     private FlightDetailsService flightDetailsService;
 
+    @Autowired
+    FlightDetailsRepository flightDetailsRepository;
+
     @PostMapping("/save")
     public ResponseEntity<FlightDetails> saveFlightDetails(@RequestBody FlightDetails flightDetails) {
         FlightDetails savedFlightDetails = flightDetailsService.saveFlightDetails(flightDetails);
         return ResponseEntity.ok(savedFlightDetails);
     }
+
+    @GetMapping("/getNewYorkORMumbai")
+    public List<FlightDetails> getFlightsFromJFK() {
+        return flightDetailsRepository.findFlightsByDepartureLocation();
+    }
+
+    //gorup
+    //matchOperation
+    //sort
+    // -- pass Aggregation
+
+
+
 }
--- src/main/java/com/test/mongodbTest/Repository/FlightDetailsRepository.java
+++ src/main/java/com/test/mongodbTest/Repository/FlightDetailsRepository.java
@@ -1,8 +1,18 @@
 package com.test.mongodbTest.Repository;
 
 import com.test.mongodbTest.Model.FlightDetails;
+import com.test.mongodbTest.Model.PartnerContainer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface FlightDetailsRepository extends MongoRepository<FlightDetails, String> {
     // You can add custom query methods if needed
+
+
+    //db["flightDetails"].find({ 'departureLocation' : 'New York (JFK)' })
+//    @Query("{ 'departureLocation' : 'New York (JFK)' }")
+    @Query("{ '$or': [ { 'departureLocation': 'New York (JFK)' }, { 'departureLocation': 'Mumbai' } ] }")
+    List<FlightDetails> findFlightsByDepartureLocation();
 }
